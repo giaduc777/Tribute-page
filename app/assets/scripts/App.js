887ed4434a5fdeaaff47b0adf17907135f6de1e5
Duc Le <giaduc777@yahoo.com>
2019-09-17 16:52:58 -0700

    1st commit

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,4 @@
+/*
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
@@ -9,3 +10,4 @@ new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
+*/
